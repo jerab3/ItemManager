d32339dd64bbb74b55367081e5300ff879278d17
Roman Jerabek <romanjerabek8@gmail.com>
2024-07-28 17:43:25 +0200

    + Added unit tests

---
+++ ItemManager.Tests/AccessoryTests.cs
@@ -0,0 +1,132 @@
+﻿using Xunit;
+using Moq;
+using Interfaces.StorageLocations;
+using Objects.Accessories;
+using Objects.ObjectProperties;
+
+namespace ItemManager.Tests
+{
+    public class AccessoryTests
+    {
+        #region StorableAccessory
+        [Fact]
+        public void StorableAccessory_Constructor_Values_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            mockStorageLocation.Setup(x => x.LocationName).Returns("Sklep");
+
+            var accessory = new StorableAccessory(0, "Test accessory", mockStorageLocation.Object);
+
+            Assert.Equal(0, accessory.Id);
+            Assert.Equal("Test accessory", accessory.AccessoryName);
+            Assert.Equal(mockStorageLocation.Object, accessory.StorageLocation);
+            Assert.Equal("Sklep", mockStorageLocation.Object.LocationName);
+        }
+
+        [Fact]
+        public void StorableAccessory_Constructor_Exceptions_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+
+            Assert.Throws<ArgumentException>(() => new StorableAccessory(1, null, mockStorageLocation.Object));
+            Assert.Throws<ArgumentException>(() => new StorableAccessory(1, "  ", mockStorageLocation.Object));
+            Assert.Throws<ArgumentException>(() => new StorableAccessory(1, "", mockStorageLocation.Object));
+            Assert.Throws<ArgumentNullException>(() => new StorableAccessory(1, "Test accessory", null));
+        }
+        #endregion
+        #region UsableStorableAccessory
+        [Fact]
+        public void UsableStorableAccessory_Constructor_BeingUsed_Values_Test()
+        {
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", "Zapojeno v PC");
+
+            Assert.Equal(0, usableAccessory.Id);
+            Assert.Equal("Kabel", usableAccessory.AccessoryName);
+            Assert.True(usableAccessory.IsBeingUsed);
+            Assert.Equal("Zapojeno v PC", usableAccessory.UsageDescription);
+        }
+
+        [Fact]
+        public void UsableStorableAccessory_Constructor_NotBeingUsed_Values_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            mockStorageLocation.Setup(x => x.LocationName).Returns("Sklep");
+
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", mockStorageLocation.Object);
+
+            Assert.Equal(0, usableAccessory.Id);
+            Assert.Equal("Kabel", usableAccessory.AccessoryName);
+            Assert.False(usableAccessory.IsBeingUsed);
+            Assert.Equal(mockStorageLocation.Object, usableAccessory.StorageLocation);
+            Assert.Equal("Sklep", mockStorageLocation.Object.LocationName);
+        }
+
+        [Fact]
+        public void UsableStorableAccessory_Constructor_Exceptions_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, null, mockStorageLocation.Object));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "", mockStorageLocation.Object));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "  ", mockStorageLocation.Object));
+
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, null, "Zapojen v PC"));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "", "Zapojen v PC"));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "  ", "Zapojen v PC"));
+
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "Kabel", usageDescription:null));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "Kabel", ""));
+            Assert.Throws<ArgumentException>(() => new UsableStoreableAccessory(0, "Kabel", "  "));
+
+            Assert.Throws<ArgumentNullException>(() => new UsableStoreableAccessory(0, "Kabel", storageLocation:null));
+        }
+       
+        [Fact]
+        public void UsableStorableAccessory_StartUsingAccessory_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", mockStorageLocation.Object);
+
+            usableAccessory.StartUsing("Pripojen k PC");
+
+            Assert.Null(usableAccessory.StorageLocation);
+            Assert.True(usableAccessory.IsBeingUsed);
+            Assert.Equal("Pripojen k PC", usableAccessory.UsageDescription);
+        }
+
+        [Fact]
+        public void UsableStorableAccessory_StartUsingAccessory_Exceptions_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", mockStorageLocation.Object);
+
+            Assert.Throws<ArgumentException>(() => usableAccessory.StartUsing(null));
+            Assert.Throws<ArgumentException>(() => usableAccessory.StartUsing("   "));
+            Assert.Throws<ArgumentException>(() => usableAccessory.StartUsing(""));
+        }
+
+        [Fact]
+        public void UsableStorableAccessory_StopUsingAccessory_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", "Pripojen k PC");
+
+            usableAccessory.StopUsing(mockStorageLocation.Object);
+
+            Assert.Null(usableAccessory.UsageDescription);
+            Assert.False(usableAccessory.IsBeingUsed);
+            Assert.Equal(usableAccessory.StorageLocation, mockStorageLocation.Object);
+        }
+
+        [Fact]
+        public void UsableStorableAccessory_StopUsingAccessory_Exceptions_Test()
+        {
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            var usableAccessory = new UsableStoreableAccessory(0, "Kabel", "Pripojen k PC");
+
+            Assert.Throws<ArgumentNullException>(() => usableAccessory.StopUsing(null));
+        }
+
+        #endregion
+    }
+}
---
+++ ItemManager.Tests/ItemTests.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using Objects.StorageLocations;
+using Objects.Items;
+using Moq;
+using Interfaces.StorageLocations;
+using Interfaces.PurchaseLocations;
+using Interfaces.Accessories;
+
+namespace ItemManager.Tests
+{
+    public class ItemTests
+    {
+        #region Item
+        [Fact]
+        public void Item_Constructor_Values_Stored_WithoutAccessories_Test()
+        {
+            var mockPurchaseLocation = new Mock<IPurchaseLocation>();
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            DateTime dateTime = DateTime.Now;
+
+            var item = new Item(0, "PC", dateTime, mockPurchaseLocation.Object, 150.99M, null, mockStorageLocation.Object);
+
+            Assert.Equal(0, item.Id);
+            Assert.Equal("PC", item.ItemName);
+            Assert.Equal(dateTime, item.PurchaseDate);
+            Assert.Equal(mockPurchaseLocation.Object, item.PurchaseLocation);
+            Assert.Equal(150.99M, item.Cost);
+            Assert.Null(item.Accessories);
+            Assert.Equal(mockStorageLocation.Object, item.StorageLocation);
+        }
+
+        [Fact]
+        public void Item_Constructor_Values_Used_WithAccessories_Test()
+        {
+            var mockPurchaseLocation = new Mock<IPurchaseLocation>();
+            var mockStorageLocation = new Mock<IStorageLocation>();
+            DateTime dateTime = DateTime.Now;
+
+            var mockAccessory1 = new Mock<IAccessory>();
+            var mockAccessory2 = new Mock<IAccessory>();
+            var mockAccessory3  = new Mock<IAccessory>();
+
+            var mockAccessories = new List<IAccessory> { mockAccessory1.Object, mockAccessory2.Object, mockAccessory3.Object};
+
+            var item = new Item(0, "PC", dateTime, mockPurchaseLocation.Object, 150.99M, mockAccessories, "In living room on table");
+
+            Assert.Equal(0, item.Id);
+            Assert.Equal("PC", item.ItemName);
+            Assert.Equal(dateTime, item.PurchaseDate);
+            Assert.Equal(mockPurchaseLocation.Object, item.PurchaseLocation);
+            Assert.Equal(150.99M, item.Cost);
+            Assert.Equal(3, item.Accessories.Count);
+            Assert.Equal("In living room on table", item.UsageDescription);
+        }
+        #endregion
+        #region WarrantyItem
+        [Fact]
+        public void WarrantyItem_IsWarrantyStillValid_Test()
+        {
+            var mockPurchaseLocation = new Mock<IPurchaseLocation>();
+            var mockStorageLocation = new Mock<IStorageLocation>();
+
+            DateTime warrantyEndTime = new DateTime(2025, 01, 01);
+
+            var warrantyItem = new WarrantyItem(0, "PC", DateTime.Now, mockPurchaseLocation.Object, 0, null, mockStorageLocation.Object, warrantyEndTime);
+
+            Assert.True(warrantyItem.IsWarrantyStillValid());
+
+            warrantyEndTime = new DateTime(2023, 01, 01);
+
+            warrantyItem = new WarrantyItem(0, "PC", DateTime.Now, mockPurchaseLocation.Object, 0, null, mockStorageLocation.Object, warrantyEndTime);
+
+            Assert.False(warrantyItem.IsWarrantyStillValid());
+        }
+        #endregion
+    }
+}
